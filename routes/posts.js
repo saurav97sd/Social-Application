d205d43fe2de6bb740a6db5a6ebb1bd43047e8a8
Saurav Das <sauravdas969.sd@gmail.com>
2021-02-26 18:31:10 +0530

    check authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport'); //to set check at router level
 
 // require postcontroller from controller folder
 const postController = require('../controllers/posts_controller');
 
 // linking action for controller with the route
-router.post('/create', postController.create);
+router.post('/create', passport.checkAuthentication, postController.create);
 
 module.exports = router;
