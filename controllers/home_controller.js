25e0d260418cfc4766354057a50352f8ab860e3a
Saurav Das <sauravdas969.sd@gmail.com>
2021-02-26 18:09:42 +0530

    display post and related user from mongodb in the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,32 @@
 // const db = require('../configs/mongoose');
+const Post = require('../models/post'); //require post model
 
 module.exports.home = function(req, res){
 
     // console.log(req.cookies); //Accessing the cookie created in browser
     // res.cookie('user-id', 25); //Updating or chnaging the cokkies value
 
-    return res.render('home', {
-        title: "Home | Authentication"
-    });
+    // find all the posts
+    // Post.find({}, function(err,posts){
+    //     if(err){
+    //         console.log('Error finding Post');
+    //     }
+    //     // else
+    //     return res.render('home', {
+    //         title: "Home | Authentication",
+    //         posts: posts
+    //     });
+    // });
+
+    // to get the user info from another schema need to prepopulate the user of post db
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err){
+            console.log('Error finding Post');
+        }
+         // else
+        return res.render('home', {
+            title: "Home | Authentication",
+            posts: posts
+        });
+    }); 
 }
