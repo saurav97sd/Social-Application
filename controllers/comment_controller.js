d6b01c03105d0842049bd8bba496845e6e9fdf25
Saurav Das <sauravdas969.sd@gmail.com>
2021-03-11 13:44:43 +0530

    Add more async await

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -2,52 +2,102 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = (req, res) => {
-    // find post if exist then add comment else not 
-    Post.findById(req.body.post, (err, post) => {
-        // handle errors
-        if(err){
-            console.log('Error finding post');
-            return;
-        }
+// module.exports.create = (req, res) => {
+//     // find post if exist then add comment else not 
+//     Post.findById(req.body.post, (err, post) => {
+//         // handle errors
+//         if(err){
+//             console.log('Error finding post');
+//             return;
+//         }
 
-        // if found then create posts
-        if(post){
-            Comment.create({
+//         // if found then create posts
+//         if(post){
+//             Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             }, function(err, comment) {
+//                 // handle errors
+//                 if(err){
+//                     console.log('Error creating comment');
+//                     return;
+//                 }
+
+//                 // if success add the comment to the post/update post
+//                 post.comments.push(comment);
+//                 post.save();
+//                 // return
+//                 res.redirect('/');
+//             });
+//         }
+//     });
+// };
+
+//adding async await to comment create 
+module.exports.create = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.body.post);
+
+        if (post){
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment) {
-                // handle errors
-                if(err){
-                    console.log('Error creating comment');
-                    return;
-                }
-
-                // if success add the comment to the post/update post
-                post.comments.push(comment);
-                post.save();
-                // return
-                res.redirect('/');
             });
+
+            post.comments.push(comment);
+            post.save();
+
+            res.redirect('/');
         }
-    });
-};
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
 
 
 // controller to delete the comments
-module.exports.destroy = function(req, res){
-    // find he comments
-    Comment.findById(req.params.id, function(err, comment){
-        // only the one who wrote can delete the comment
-        if(comment.user == req.user.id){
+// module.exports.destroy = function(req, res){
+//     // find he comments
+//     Comment.findById(req.params.id, function(err, comment){
+//         // only the one who wrote can delete the comment
+//         if(comment.user == req.user.id){
+//             let postId = comment.post;
+//             comment.remove();
+//             Post.findById(postId, {$pull : {comments: req.params.id}} ,function(err,post){
+//                 return res.redirect('back');
+//             });
+//         }else{
+//             return res.redirect('back');
+//         }
+//     });
+// };
+
+//adding async awiat to deleting comment
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
-            Post.findById(postId, {$pull : {comments: req.params.id}} ,function(err,post){
-                return res.redirect('back');
-            });
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            return res.redirect('back');
         }else{
             return res.redirect('back');
         }
-    });
-};
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,37 +2,78 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    // Storing in db
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
+// module.exports.create = function(req, res){
+//     // Storing in db
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
         
-    }, function(err, post){
-        if(err){
-            console.log('Error Creating Post');
-            return;
-        }
-        console.log('Post Created');
-        return res.redirect('back');
-    });
-};
+//     }, function(err, post){
+//         if(err){
+//             console.log('Error Creating Post');
+//             return;
+//         }
+//         console.log('Post Created');
+//         return res.redirect('back');
+//     });
+// };
 
-module.exports.destroy = function(req, res){
-    // check if post exist in the db
-    Post.findById(req.params.id, function(err,post){
-        // if exist
-        // Only user who wrote the post can delete it
-        if(post.user == req.user.id){
-            // .id means converting the object id into string
+// adding async wait to post create
+module.exports.create = async function(req, res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+    
+        return res.redirect('back');
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+  
+}
+
+// module.exports.destroy = function(req, res){
+//     // check if post exist in the db
+//     Post.findById(req.params.id, function(err,post){
+//         // if exist
+//         if(err){
+//             console.log('Post Not found');
+//         }
+//         // Only user who wrote the post can delete it
+//         if(post.user == req.user.id){
+//             // .id means converting the object id into string
+//             post.remove();
+
+//             Comment.deleteMany({post : req.params.id}, function(err){
+//                 return res.redirect('back');
+//             });
+//         }else{
+//             // if post not found
+//             return res.redirect('back');
+//         }
+//     });
+// }
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
             post.remove();
 
-            Comment.deleteMany({post : req.params.id}, function(err){
-                return res.redirect('back');
-            });
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
         }else{
-            // if post not found
             return res.redirect('back');
         }
-    });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
 }
