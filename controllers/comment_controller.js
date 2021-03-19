ddb27029577b16ace400de607b03c0a1ac8dfe39
Saurav Das <sauravdas969.sd@gmail.com>
2021-03-19 10:20:40 +0530

    Use Ajax for creating/deleting comments dynamically

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -35,6 +35,32 @@ const Post = require('../models/post');
 // };
 
 //adding async await to comment create 
+// module.exports.create = async function(req, res){
+
+//     try{
+//         let post = await Post.findById(req.body.post);
+
+//         if (post){
+//             let comment = await Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             });
+
+//             post.comments.push(comment);
+//             post.save();
+
+//             // flash message for comments create
+//             req.flash('success', 'Comment Published successfully !!');
+//             return res.redirect('/');
+//         }
+//     }catch(err){
+//         req.flash('error', err);
+//         return res.redirect('back');
+//     }
+    
+// }
+// Checking for xhr request of ajax method
 module.exports.create = async function(req, res){
 
     try{
@@ -50,13 +76,26 @@ module.exports.create = async function(req, res){
             post.comments.push(comment);
             post.save();
 
-            // flash message for comments create
-            req.flash('success', 'Comment Published successfully !!');
-            return res.redirect('/');
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
         }
     }catch(err){
         req.flash('error', err);
-        return res.redirect('back');
+        return;
     }
     
 }
@@ -80,6 +119,33 @@ module.exports.create = async function(req, res){
 // };
 
 //adding async awiat to deleting comment
+// module.exports.destroy = async function(req, res){
+
+//     try{
+//         let comment = await Comment.findById(req.params.id);
+
+//         if (comment.user == req.user.id){
+
+//             let postId = comment.post;
+
+//             comment.remove();
+
+//             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+//             // flash message for deleting commit
+//             req.flash('success','Comment Deleted :)');
+//             return res.redirect('back');
+//         }else{
+//             req.flash('error','You cant delete the comment !!');
+//             return res.redirect('back');
+//         }
+//     }catch(err){
+//         req.flash('error', err);
+//         return res.redirect('back');
+//     }
+    
+// }
+
 module.exports.destroy = async function(req, res){
 
     try{
@@ -93,16 +159,27 @@ module.exports.destroy = async function(req, res){
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
 
-            // flash message for deleting commit
-            req.flash('success','Comment Deleted :)');
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
         }else{
-            req.flash('error','You cant delete the comment !!');
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     }catch(err){
         req.flash('error', err);
-        return res.redirect('back');
+        return;
     }
     
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -28,6 +28,10 @@ module.exports.create = async function(req, res){
 
         // check if ajax request 
         if(req.xhr){
+
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
+            
             return res.status(200).json({
                 data: {
                     post: post
